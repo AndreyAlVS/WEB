588acc674a3606cc48977fd3d1eb798ff68f2461
AndreyAlVS <aavasquezs@gmail.com>
2023-11-07 12:14:55 +0300

    HW CV

--- Homeworks/HWS2_191023/js/calc.js
+++ Homeworks/HWS2_191023/js/calc.js
@@ -3,19 +3,25 @@ function calc() {
     let age = document.getElementById("age").value
     let portion = ""
 
-    if (weight <= 10 && age <= 1) {
-        portion = "У вас точно собака? Удвойте порцию!"
-    }
-    else if (weight <= 10 && age >1) {
-        portion = "Поздравляю, у вас хомяк! Дайте ему морковку."
-    }
-    else if (weight > 10 && age > 1) {
-        portion = "Отличная собака, отличная порция!" 
-    }
-    else {
-        portion = "Некорректный ввод" 
-    }
     
-    document.getElementById("portion").value = portion
+    switch(true) {
+        case (weight <= 10 && age <= 1): portion = "У вас точно собака? Удвойте порцию!"; break;
+        case (weight <= 10 && age >1): portion = "Поздравляю, у вас хомяк! Дайте ему морковку."; break;
+        case (weight > 10 && age > 1): portion = "Отличная собака, отличная порция!"; break;
+     }
+//     if (weight <= 10 && age <= 1) {
+//         portion = "У вас точно собака? Удвойте порцию!"
+//     }
+//     else if (weight <= 10 && age >1) {
+//         portion = "Поздравляю, у вас хомяк! Дайте ему морковку."
+//     }
+//     else if (weight > 10 && age > 1) {
+//         portion = "Отличная собака, отличная порция!" 
+//     }
+//     else {
+//         portion = "Некорректный ввод" 
+//     }
+    
+document.getElementById("portion").value = portion
 }
 
